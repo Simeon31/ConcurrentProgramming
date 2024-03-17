38bb10976c7f84eef6baed56a38bebae91a22314
Simeon Markov <simeonmarkov777@gmail.com>
2024-03-17 14:50:32 +0200

    threading

---
+++ Threading/Example01/FirstThread.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    class FirstThread
+    {
+        public void DoTask1()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Thread1:job({0})", i); // Prints the current iteration number for the first thread to the console
+
+            }
+        }
+        public void DoTask2()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Thread2:job({0})", i); // Prints the current iteration number for the second thread to the console
+            }
+        }
+
+    }
+}
---
+++ Threading/Example01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FirstThread ft = new FirstThread(); // An object of the FirstThread class is created
+            Thread t1 = new Thread(new ThreadStart(ft.DoTask1)); // Creates a first thread that will execute the DoTask1 method
+            Thread t2 = new Thread(new ThreadStart(ft.DoTask2)); // Creates a second thread that will execute the DoTask2 method
+
+            t1.Start(); // The first thread is started
+            t2.Start(); // The second thread is started
+        }
+    }
+}
